7ddf79883033be6225ad8371aa8aac8c88a98d65
antonkornilov-ua <anton.kornilov@hotmail.com>
2024-01-01 20:23:27 +0200

    Server work func

--- src/app.js
+++ src/app.js
@@ -1,10 +1,17 @@
 const http = require('http');
+const url = require('url');
 
-const serverFunc = () => {
+const serverFunc = (req, res) => {
+    const { pathname, query } = url.parse(req.url, true);
+    switch (pathname) {
+        case '/pathname': {
+            
+            break;
+        }
+    }
+    res.end('Hello');
+};
 
-}
-
-
-const app = http.createServer(serverFunc)
+const app = http.createServer(serverFunc);
 
 module.exports = app;
