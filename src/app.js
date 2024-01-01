1995b71606a7e8c45114ea4eb460d5a7443206d1
antonkornilov-ua <anton.kornilov@hotmail.com>
2024-01-01 21:56:37 +0200

    converter func

--- src/app.js
+++ src/app.js
@@ -1,15 +1,20 @@
 const http = require('http');
 const url = require('url');
+const converter = require('./converter');
 
 const serverFunc = (req, res) => {
     const { pathname, query } = url.parse(req.url, true);
+    let respText = 'idk';
+    
+
+
     switch (pathname) {
-        case '/pathname': {
-            
+        case '/converter': {
+            respText = converter(query.value, query.to);
             break;
         }
     }
-    res.end('Hello');
+    res.end(String(respText));
 };
 
 const app = http.createServer(serverFunc);
---
+++ src/converter.js
@@ -0,0 +1,10 @@
+const KG_LBS_MULTI = 2.2046;
+
+const converter = (value, to) => {
+    
+
+    if (to === 'lbs') return value * KG_LBS_MULTI;
+    if (to === 'kg') return value / KG_LBS_MULTI;
+    return 0;
+};
+module.exports = converter;
