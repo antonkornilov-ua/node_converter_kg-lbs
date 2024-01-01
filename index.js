5c82366b2cc7bdc97318760e3959d0426e3d607f
antonkornilov-ua <anton.kornilov@hotmail.com>
2024-01-01 15:09:46 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const app = require('./src/app');
+const PORT = 3000;
+const startServerText = `Server started. URL: http:localhost:${PORT}`
+
+
+app.listen(PORT, () => console.log(startServerText));
